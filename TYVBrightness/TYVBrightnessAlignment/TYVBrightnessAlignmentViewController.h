94dc6b4a3fb689cabb6ba5dcb8bd6ab04ecd9976
Yurii Trokhymchuk <trokhymchuk@gmail.com>
2018-07-13 22:06:33 +0300

    TYVBrightnessAlignmentViewController -> create

---
+++ TYVBrightness/TYVBrightnessAlignment/TYVBrightnessAlignmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TYVBrightnessAlignmentViewController.h
+//  TYVBrightness
+//
+//  Created by Yurii Trokhymchuk on 13.07.18.
+//  Copyright © 2018 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TYVBrightnessAlignmentViewController : NSViewController
+
+@end
---
+++ TYVBrightness/TYVBrightnessAlignment/TYVBrightnessAlignmentViewController.m
@@ -0,0 +1,22 @@
+//
+//  TYVBrightnessAlignmentViewController.m
+//  TYVBrightness
+//
+//  Created by Yurii Trokhymchuk on 13.07.18.
+//  Copyright © 2018 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import "TYVBrightnessAlignmentViewController.h"
+
+@interface TYVBrightnessAlignmentViewController ()
+
+@end
+
+@implementation TYVBrightnessAlignmentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
